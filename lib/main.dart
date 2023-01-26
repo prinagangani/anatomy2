878ded5a55bba8b942b5a089dc801158eb287ce4
country-list <prinagangani10@gmail.com>
2023-01-26 16:44:45 +0530

    .

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,16 @@
 import 'package:flutter/material.dart';
+
 void main()
 {
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: SafeArea(
-        child: Scaffold(
-          appBar: AppBar(
-            backgroundColor: Colors.red,
-            leading: Icon(Icons.menu),
-            title: Text("Flutter App"),centerTitle: true,
-          ),
-          body: Center(
-            child: Text("    Red and White Group of Institutes\nOne step in Changing Education Chain...",style: TextStyle(fontSize: 20,color: Colors.red),),
+      home: Scaffold(
+        backgroundColor: Colors.black,
+        body: Center(
+          child: Text(
+            "Hello\n\n\n\n\n\nDart\n\n\n\n\n\nFlutter",
+            style: TextStyle(fontSize: 25,color: Colors.red,fontWeight: FontWeight.w500,letterSpacing: 4,decoration: TextDecoration.underline,decorationColor: Colors.yellow[600]),
           ),
         ),
       ),
